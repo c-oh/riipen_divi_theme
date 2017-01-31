3f591d13c4348513e4430efe0cdf95014e6a5f16
hollythedev <papiniu@outlook.com>
2017-01-31 13:31:27 -0800

    Added mailchimp email

--- new_theme/sidebar-footer.php
+++ new_theme/sidebar-footer.php
@@ -18,17 +18,10 @@
 	?>
 			</div>
 			<!-- Begin MailChimp Signup Form -->
-			<div id="mc_embed_signup">
-				<form action="//riipen.us7.list-manage.com/subscribe/post?u=eb86e81b0b5260b837636d6db&amp;id=91ad500c12" method="post" id="mc-embedded-subscribe-form"
-								name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<div id="mc_embed_signup_scroll">
-						<label for="mce-EMAIL">Be Updated!</label>
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_eb86e81b0b5260b837636d6db_91ad500c12" tabindex="-1" value=""></div>
-						<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					</div>
-				</form>
+			<div id="mailchimp">
+				
+			<?php echo do_shortcode( '[yikes-mailchimp form="1"]' ); ?>
+
 			</div>
 			<!--End mc_embed_signup-->
 			<!-- #footer-widgets -->
